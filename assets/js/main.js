c14ade944a543a572d27b1e4819c76d9fc965388
umut <askarbekova.umut.kal@gmail.com>
2022-01-27 14:45:48 +0600

    init files

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+searchField();
+menuMob();
+window.onload = function (){
+   /* $('.str-slider-dapp').liMarquee({
+        direction: 'left',
+        circular: true,
+        loop: -1,
+        hoverStop: true,
+        startShow: true,
+        scrollStop: true
+    });
+    $('.str-slider-cat').liMarquee({
+        direction: 'left',
+        circular: true,
+        loop: -1,
+        startShow: true,
+        hoverStop: true,
+        scrollStop: true,
+        scrollDelay: 200
+    });
+    $('.str-slider-gallery').liMarquee({
+        direction: 'left',
+        circular: true,
+        loop: -1,
+        hoverStop: true,
+        startShow: true,
+        scrollStop: true
+    });
+
+
+    $('.mfp-arrow-left').click(function () {
+        $('.str-slider-gallery').liMarquee('changeOptions',{direction: 'left'});
+    });
+    $('.mfp-arrow-right').click(function () {
+        $('.str-slider-gallery').liMarquee('changeOptions',{direction: 'right'});
+    });*/
+
+};
